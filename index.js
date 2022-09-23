56be15107ded98ec5414c00213c593f040032bc6
Abdullah Sajjad <112166581+AbdullahSajjad216@users.noreply.github.com>
2022-09-23 18:58:20 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).click();
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 2500);
+
+document.getElementById("mymodal").addEventListener("click", ()=>{
+  document.getElementById("hide_btn_js").click();
+});// To close the contact us form by clicking anywhere
 
 
 
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+	});
+});
